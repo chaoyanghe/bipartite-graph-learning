fe2620c489dcb9693135462e4ed906f50fc828de
Chaoyang He <me@chaoyanghe.com>
2019-04-27 23:31:04 -0700

    debug

--- Node2Vec/run_node2vec.sh
+++ Node2Vec/run_node2vec.sh
@@ -2,8 +2,8 @@
 # 清理临时文件
 #rm -r out
 # 设置库环境
-source set_dynamic_lib.sh
+source ./set_dynamic_lib.sh
 # 运行算法
-python gen_emb.py
-python demo.py
+python ./gen_emb.py
+python ./demo.py
 
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import sys
-from os.path import abspath, join, dirname
-sys.path.insert(0, join(abspath(dirname(__file__)), 'Node2Vec'))
-print(sys.path)
+# import sys
+# from os.path import abspath, join, dirname
+# sys.path.insert(0, join(abspath(dirname(__file__)), 'Node2Vec'))
+# print(sys.path)
