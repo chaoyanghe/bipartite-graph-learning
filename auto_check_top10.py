8e3b1f086152f8e58843acc949114b7603ba7b8a
Chaoyang He <me@chaoyanghe.com>
2019-05-06 01:50:51 -0700

    Update auto_check_top10.py

--- auto_check_top10.py
+++ auto_check_top10.py
@@ -11,7 +11,7 @@ def topten(auc_dict, paras):
     print("HGCN. The top 10 AUC will be:")
     cnt = 0
     for key in auc_dict.keys():
-        print("auc = %f" % auc_dict_sorted[key])
+        print(auc_dict_sorted[key])
         (batch_size, epochs, lr, weight_decay, dis_hidden, dropout) = paras[key]
         str = "--batch_size %d --epochs %d --lr %f --weight_decay %f --dis_hidden %d --dropout %f" % (
             batch_size,
