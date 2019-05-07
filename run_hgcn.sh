b200a78e488a18fe088209fad3b12216ad7652c5
TianXieUSC <xiet@usc.edu>
2019-05-07 15:42:29 -0700

    debug

--- run_hgcn.sh
+++ run_hgcn.sh
@@ -21,19 +21,12 @@ python3 ./HGCN/hgcn_main.py \
 --model decoder_gcn \
 --gpu False \
 --batch_size 500 \
-<<<<<<< HEAD
+
 --epochs 1 \
 --lr 0.0003 \
 --weight_decay 0.0005 \
 --dis_hidden 32 \
 --dropout 0.5 \
 --gcn_output_dim 10
-=======
---epochs 3 \
---lr 0.0003 \
---weight_decay 0.001 \
---dis_hidden 16 \
---dropout 0.4
->>>>>>> 5f0a5da07742aab45882830270484d354387eb5e
 
 python3 ./HGCN/classification.py
