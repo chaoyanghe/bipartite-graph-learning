6637f76af5237426c2a06f6108ae56e3740013d4
Chaoyang He <me@chaoyanghe.com>
2019-05-18 14:37:26 -0700

    debug node2vec

--- run_node2vec.sh
+++ run_node2vec.sh
@@ -7,11 +7,11 @@ python3 ./Node2Vec/gen_emb.py --dataset $DATASET
 if [ "$DATASET" = "tencent" ]
 then
     echo $DATASET
-    python3 ./Node2Vec/multi_classification.py --dataset $DATASET
+    python3 ./Node2Vec/binary_classification.py --dataset $DATASET
 elif [ "$DATASET" = "cora" ]
 then
     echo $DATASET
-    python3 ./Node2Vec/binary_classification.py --dataset $DATASET
+    python3 ./Node2Vec/multi_classification.py --dataset $DATASET
 elif [ "$DATASET" = "citeseer" ]
 then
     echo $DATASET
