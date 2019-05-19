0b20e0efe13598253a76876a41593d033c7fafcd
Chaoyang He <me@chaoyanghe.com>
2019-05-19 10:52:05 -0700

    optimize node2vec

--- Node2Vec/binary_classification.py
+++ Node2Vec/binary_classification.py
@@ -44,15 +44,11 @@ if __name__ == "__main__":
 	emb_file = os.path.join(output_folder, "./%s.emb" % (method))
 	logging.info("This is the demo for logistic regression using the embedding vectors")
 
-	# the HGCN node list is smaller than the raw node list because some illegal nodes are filtered
-	hgcn_node_file = str(input_folder) + "/hgcn_node_list"
-	print(str(input_folder))
-
 	# Performing example logistic regression
 	if os.path.exists(emb_file):
 		max_iter = 300
-		lr_cmd = "python3 ./classifier/logistic_regression.py --verbose 0 --input_folder %s --emb_file %s --node_file %s --res_file %s --max_iter %d" % (
-			input_folder, emb_file, hgcn_node_file, res_file, it)
+		lr_cmd = "python3 ./classifier/logistic_regression.py --verbose 0 --input_folder %s --emb_file %s --res_file %s --max_iter %d" % (
+			input_folder, emb_file, res_file, it)
 
 		os.system(lr_cmd)
 	else:
