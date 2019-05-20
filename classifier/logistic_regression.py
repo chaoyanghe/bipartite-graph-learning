b269d3dd4ca1776e78d2c29c87585c75a759cca0
Chaoyang He <me@chaoyanghe.com>
2019-05-19 20:06:12 -0700

    change back to macro

--- classifier/logistic_regression.py
+++ classifier/logistic_regression.py
@@ -212,15 +212,15 @@ def run_exp(input_folder, emb_file, args):
         test_predict_y = clf.predict(test_x)
 
         # metric
-        test_precision = precision_score(test_y, test_predict_y, average="micro")
+        test_precision = precision_score(test_y, test_predict_y, average="macro")
         print("test_precision = %f" % test_precision)
         logging.info("test_precision = %f" % test_precision)
 
-        test_recall = recall_score(test_y, test_predict_y, average="micro")
+        test_recall = recall_score(test_y, test_predict_y, average="macro")
         print("test_recall = %f" % test_recall)
         logging.info("test_recall = %f" % test_recall)
 
-        test_micro_f1 = f1_score(test_y, test_predict_y, average="micro")
+        test_micro_f1 = f1_score(test_y, test_predict_y, average="macro")
         print("test_macro_f1 = %f" % test_micro_f1)
         logging.info("test_macro_f1 = %f" % test_micro_f1)
 
