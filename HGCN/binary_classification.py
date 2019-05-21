5d396fb280324918cf27fe6b619cb973f04ec694
Chaoyang He <me@chaoyanghe.com>
2019-05-21 01:00:41 -0700

    submit the result

--- HGCN/binary_classification.py
+++ HGCN/binary_classification.py
@@ -34,7 +34,7 @@ if __name__ == "__main__":
 
 		if model == "gan":
 			output_folder = "/mnt/shared/home/bipartite-graph-learning/out/hgcn-gan/" + str(dataset) + "/" + str(rank)
-		elif model == "gae":
+		elif model == "vae":
 			output_folder = "/mnt/shared/home/bipartite-graph-learning/out/hgcn-vae/" + str(dataset) + "/" + str(rank)
 
 	print("output_folder = %s" % output_folder)
